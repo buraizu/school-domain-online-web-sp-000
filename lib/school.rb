4d6afb6ea36efe24584f98a4fe905d2a8e251af9
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-10 03:01:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name
+  end
+
+end
